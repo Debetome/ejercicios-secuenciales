75353192d13fc11858eebfa528538c5d2b670ea6
Debetome <magregory@tutanota.com>
2022-11-06 18:12:59 -0500

    Haciendo ejercicio 11

--- ejercicios de benjamin/EjercicioOnce.java
+++ ejercicios de benjamin/EjercicioOnce.java
@@ -2,40 +2,44 @@ import java.util.Scanner;
 import java.util.HashMap;
 import java.util.Map;
 
-public class EjercicioOnce {
-    static float day = 86400;
-    static float hour = 3600;
-    static float minute = 60;
-
-    public static HashMap<String, Float> calculate(float time) {
-        HashMap<String, Float> result = new HashMap<String, Float>();
-
-        float minutes = time / minute;
-        float hours = time / hour;
-        float days = time / day;
-
-        result.put("minutes", minutes);
-        result.put("hours", hours);
-        result.put("days", days);
-
-        return result;
+public class EjercicioDies {
+    public static int prompt(String text, Scanner sc) {
+        int number;
+        System.out.print("[*] " + text + ": ");
+        number = sc.nextInt();
+        return number;
     }
 
-    public static void displayResult(HashMap<String, Float> result) {
-        System.out.println("\n[*] Days: " + result.get("days"));
-        System.out.println("[*] Hours: " + result.get("hours"));
-        System.out.println("[*] Minutes: " + result.get("minutes"));
+    public static HashMap<String, String> flip(HashMap<String, String> numbers) {
+        HashMap<String, String> newNumbers = new HashMap<String, String>();
+        String[] firstSplitted = numbers.get("first").split("");
+        String[] secondSplitted = numbers.get("second").split("");
+
+        newNumbers.put("first", firstSplitted[2] + secondSplitted[1] + firstSplitted[0]);
+        newNumbers.put("second", secondSplitted[2] + firstSplitted[1] + secondSplitted[0]);
+
+        return newNumbers;
     }
 
-    public static void main(String[] args) {
+    public static void displayNumbers(HashMap<String, String> numbers) {
+        System.out.println("\n[*] First number (flipped): " + numbers.get("first"));
+        System.out.println("[*] Second number (flipped): " + numbers.get("second"));
+    }
+
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        HashMap<String, Float> result;
-        float time;
+        HashMap<String, String> numbers = new HashMap<String, String>();
 
-        System.out.print("[*] Time: ");
-        time = sc.nextFloat();
-        
-        result = calculate(time);
-        displayResult(result);
+        numbers.put("first", String.valueOf(prompt("First number", sc)));
+        numbers.put("second", String.valueOf(prompt("Second number", sc)));
+
+        for (Map.Entry<String, String> item : numbers.entrySet()) {
+            if (item.getValue().length() > 3 || item.getValue().length() < 3) {
+                System.out.println("\n[-] Numbers can't either be greater or less than digit numbers!");
+                System.exit(-1);
+            }
+        }
+
+        displayNumbers(flip(numbers));
     }
 }
