4b599fe4a9e92e1bc85d7a0817c7083dc363409f
Debetome <magregory@tutanota.com>
2022-11-06 18:09:51 -0500

    Haciendo ejercicio 11

---
+++ ejercicios de benjamin/EjercicioOnce.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EjercicioOnce {
+    static float day = 86400;
+    static float hour = 3600;
+    static float minute = 60;
+
+    public static HashMap<String, Float> calculate(float time) {
+        HashMap<String, Float> result = new HashMap<String, Float>();
+
+        float minutes = time / minute;
+        float hours = time / hour;
+        float days = time / day;
+
+        result.put("minutes", minutes);
+        result.put("hours", hours);
+        result.put("days", days);
+
+        return result;
+    }
+
+    public static void displayResult(HashMap<String, Float> result) {
+        System.out.println("\n[*] Days: " + result.get("days"));
+        System.out.println("[*] Hours: " + result.get("hours"));
+        System.out.println("[*] Minutes: " + result.get("minutes"));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<String, Float> result;
+        float time;
+
+        System.out.print("[*] Time: ");
+        time = sc.nextFloat();
+        
+        result = calculate(time);
+        displayResult(result);
+    }
+}
