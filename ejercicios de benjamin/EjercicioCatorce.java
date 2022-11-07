ff4b077dc4a803ca291963440618d182ec8a7ef3
Debetome <magregory@tutanota.com>
2022-11-06 21:43:52 -0500

    Corrigiendo ejercicio catorce

---
+++ ejercicios de benjamin/EjercicioCatorce.java
@@ -0,0 +1,83 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class EjercicioCatorce {
+    static int hour = 3600;
+    static int minute = 60;
+
+    public static int prompt(String text, Scanner sc) {
+        int value;
+        System.out.print("[*] " + text + ": ");
+        value = sc.nextInt();
+        return value;
+    }
+
+    public static HashMap<String, Integer> calculate(List<HashMap<String, Integer>> times) {
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+        int total_seconds = 0;
+
+        for (HashMap<String, Integer> time : times) {
+            if (time.get("hours") >= 24) {
+                System.out.println("\n[-] Hours cannot be greater than 23");
+                return null;
+            }
+
+            if (time.get("minutes") >= 60 || time.get("seconds") >= 60) {
+                System.out.println("\n[-] Minutes and seconds cannot be greater than 59");
+                return null;
+            }
+        }
+
+        for (HashMap<String, Integer> time : times) {
+            total_seconds += time.get("hours") * hour;
+            total_seconds += time.get("minutes") * minute;
+        }
+
+        int hours = (total_seconds / 2) / hour;
+        int minutes = (total_seconds - hours * hour) / minute;
+        int seconds = total_seconds - (hours * hour + minutes * minute);
+
+        result.put("hours", hours);
+        result.put("minutes", minutes);
+        result.put("seconds", seconds);
+
+        return result;
+    } 
+
+    public static void displayResult(HashMap<String, Integer> result) {
+        int hours = result.get("hours");
+        int minutes = result.get("minutes");
+        int seconds = result.get("seconds");
+
+        String time = hours + ":" + minutes + ":" + seconds;
+
+        System.out.println("\n[*] Total in time format: " + time);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<HashMap<String, Integer>> times = new ArrayList<HashMap<String, Integer>>();
+        HashMap<String, Integer> result;
+
+        for (int i=0; i<2; i++) {
+            HashMap<String, Integer> newTime = new HashMap<String, Integer>();
+            System.out.println("\n[*] Time " + i+1);
+            
+            newTime.put("hours", prompt("Hours", sc));
+            newTime.put("minutes", prompt("Minutes", sc));
+            newTime.put("seconds", prompt("Seconds", sc));
+
+            times.add(newTime);
+        }
+
+        result = calculate(times);
+        if (result == null) {
+            System.exit(-1);
+        }
+
+        displayResult(result);
+    }
+}
